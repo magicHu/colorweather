dae5d950c470134561b9d453ed7eff169da6db7a
magic <huronghai2008@gmail.com>
2013-05-02 13:29:53 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colorweather::Application.config.secret_token = 'd6193d4cfdaac670fdcaedbd10d64058e9d9fe4e3884354d6072a52341dc30f4f682594f693c05d0fba072cf44e2fa4019108b089f3f44d3b082e35938ba937e'
